98ebb7d1f2dda16e6cf280f8598b0b817473e912
lonesloane <stephane.varin@gmail.com>
2016-04-13 13:15:54 +0200

    cosmetic changes

--- numbers/Random.py
+++ numbers/Random.py
@@ -3,15 +3,15 @@ import random
 
 def randomChoice():
     sequence = ["bleu", "blanc", "rouge"]
-    for i in range(1,10):
+    for i in range(1, 10):
         print(random.choice(sequence))
 
 def randomRange():
-    for i in range(1,10):
-        print(random.randrange(0,100,5))
+    for i in range(1, 10):
+        print(random.randrange(0, 100, 5))
 
 def randomSample():
-    print random.sample(xrange(1,50), 15)
+    print random.sample(xrange(1, 50), 15)
 
 if __name__ == "__main__":
     randomChoice()
